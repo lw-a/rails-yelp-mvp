8ecaaf79ad018dda292f20049fc0fee43f9ed239
Adam Hemming <hemming.dev@gmail.com>
2022-08-11 02:20:53 +0900

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning DB'
+
+Restaurant.destroy_all
+
+puts 'Seeding restaurants'
+
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: Restaurant::CATEGORY.sample
+  )
+end
+
+puts "Finished Seeding"
